172a8e425eb7556dd98f109e5e49f95cbd24f63d
lawvs <18554747+lawvs@users.noreply.github.com>
2020-11-20 05:47:11 +0900

    chore: update project settings

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,9 @@ import type { Config } from '@jest/types'
 
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
+  testTimeout: 50,
+  testMatch: ['**/?(*.)+(spec|test).ts?(x)'],
+  collectCoverageFrom: ['src/**/*.{ts,tsx}'],
 }
 
 export default config
