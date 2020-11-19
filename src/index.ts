3bebcf4b5c53e226d0bf5eecc447e9f420628bd6
lawvs <18554747+lawvs@users.noreply.github.com>
2020-11-20 02:48:41 +0900

    feat: support catch

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,21 @@ type Callback = (value: any) => any
 export class Promise {
   private state = 'pending' as 'pending' | 'resolved' | 'rejected'
   private pendThen?: Callback
+  private pendCatch?: Callback
   static resolve: (val: any) => Promise
   static reject: (val: any) => Promise
 
-  constructor(fn: (res: Promise['resolveValue']) => void) {
-    fn((val) => this.resolveValue(val))
+  constructor(
+    fn: (res: Promise['resolveValue'], rej: Promise['reject']) => void
+  ) {
+    try {
+      fn(
+        (val) => this.resolveValue(val),
+        (e) => this.reject(e)
+      )
+    } catch (error) {
+      this.pendCatch?.(error)
+    }
   }
 
   private resolveValue(val?: any) {
@@ -17,15 +27,46 @@ export class Promise {
     this.pendThen?.(val)
   }
 
+  private reject(e?: Error) {
+    if (this.state === 'pending') {
+      this.state = 'rejected'
+    }
+    this.pendCatch?.(e)
+  }
+
   then(fn: Callback) {
-    return new Promise((res) => {
+    return new Promise((res, rej) => {
       // defer run after this promise resolved
       this.pendThen = (val) => {
-        const resVal = fn(val)
-        if (typeof resVal?.then === 'function') {
-          resVal.then((v: any) => res(v))
-        } else {
-          res(resVal)
+        try {
+          const resVal = fn(val)
+          if (typeof resVal?.then === 'function') {
+            resVal.then((v: any) => res(v))
+          } else {
+            res(resVal)
+          }
+        } catch (error) {
+          // TODO throw promiseLike?
+          rej(error)
+        }
+      }
+    })
+  }
+
+  catch(fn: Callback) {
+    return new Promise((res, rej) => {
+      // defer run after this promise rejected
+      this.pendCatch = (val) => {
+        try {
+          const resVal = fn(val)
+          if (typeof resVal?.then === 'function') {
+            resVal.then((v: any) => res(v))
+          } else {
+            res(resVal)
+          }
+        } catch (error) {
+          // TODO throw promiseLike?
+          rej(error)
         }
       }
     })
@@ -33,3 +74,4 @@ export class Promise {
 }
 
 Promise.resolve = (val: any) => new Promise((res) => res(val))
+Promise.reject = (val: any) => new Promise((_, rej) => rej(val))
