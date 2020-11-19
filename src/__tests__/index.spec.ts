5055fbfa6d2418f0c5684e55641d4d3da4106229
lawvs <18554747+lawvs@users.noreply.github.com>
2020-11-20 02:29:21 +0900

    tests: update tests

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -1,14 +1,21 @@
 import { Promise as MyPromise } from '..'
 
-jest.useFakeTimers()
-
 function delay(ms: number) {
   return new MyPromise((res) => setTimeout(() => res(), ms))
 }
 
+const spyLog = jest.spyOn(console, 'log')
+
+beforeAll(() => {
+  jest.useFakeTimers()
+})
+
+beforeEach(() => {
+  spyLog.mockReset()
+})
+
 describe('base', () => {
-  test('should promise works', async () => {
-    const spy = jest.spyOn(console, 'log')
+  test('should promise works', () => {
     delay(1000)
       .then(() => '123')
       .then((val) => console.log(val))
@@ -21,16 +28,16 @@ describe('base', () => {
     expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 1000)
     jest.runOnlyPendingTimers()
 
-    expect(spy).toHaveBeenNthCalledWith(1, '123')
-    expect(spy).toHaveBeenNthCalledWith(2, 'A')
-    expect(spy).toHaveBeenNthCalledWith(3, 'B')
+    expect(spyLog).toHaveBeenNthCalledWith(1, '123')
+    expect(spyLog).toHaveBeenNthCalledWith(2, 'A')
+    expect(spyLog).toHaveBeenNthCalledWith(3, 'B')
 
     expect(setTimeout).toBeCalled()
     expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 2000)
     expect(setTimeout).toHaveBeenCalledTimes(2)
     jest.runOnlyPendingTimers()
 
-    expect(spy).toHaveBeenNthCalledWith(4, 'C')
-    expect(spy).toHaveBeenCalledTimes(4)
+    expect(spyLog).toHaveBeenNthCalledWith(4, 'C')
+    expect(spyLog).toHaveBeenCalledTimes(4)
   })
 })
